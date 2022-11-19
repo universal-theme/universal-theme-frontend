c0f203150e451d0a54305936d6a60be91fa9ec3f
Roy Stewart <roythomasstewart@gmail.com>
2022-11-19 11:42:31 -0500

    Added a current theme link to the home page

--- src/main-app/home.jsx
+++ src/main-app/home.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
-    const activeTheme = "Default Theme";
+    const activeTheme = {
+        name: "Default Theme",
+        id: 0,
+    };
     return <div>
-        <span>{activeTheme}</span>
+        current theme: <Link to={`/edit/${activeTheme.id}`}>{activeTheme.name}</Link>
     </div>;
 };
