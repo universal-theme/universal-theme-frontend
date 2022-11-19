2f1db98482e341cde7d15f0a4e55cd0cb57505dd
Roy Stewart <roythomasstewart@gmail.com>
2022-11-19 09:55:02 -0500

    Added basic inputs to the theme editor

--- src/main-app/theme-editor.jsx
+++ src/main-app/theme-editor.jsx
@@ -1,3 +1,31 @@
+import { Input, FormControl, FormLabel, Paper, TextField, InputAdornment, Button } from "@mui/material";
+
 export const ThemeEditor = () => {
-    return null;
+    return <Paper sx={{display: "flex", "flex-direction": "column"}}>
+        <TextField required label="Name" />
+        
+        <FormControl fullwidth>
+            <FormLabel>Primary color</FormLabel>
+            <Input type="color" />
+        </FormControl>
+
+        <FormControl fullwidth>
+            <FormLabel>Secondary color</FormLabel>
+            <Input type="color" />
+        </FormControl>
+
+        <FormControl fullwidth>
+            <FormLabel>Error color</FormLabel>
+            <Input type="color" defaultValue="#ff0000" />
+        </FormControl>
+
+        <FormControl fullwidth>
+            <FormLabel>Warning color</FormLabel>
+            <Input type="color" defaultValue="#ff8800" />
+        </FormControl>
+
+        <TextField required label="Font size" InputProps={{ endAdornment: <InputAdornment position="end">px</InputAdornment>}} defaultValue="12" />
+
+        <Button>Save</Button>
+    </Paper>;
 };
