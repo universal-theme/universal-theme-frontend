22ba2aa471e83d40132a29fb84305ec32a412bca
Roy Stewart <roythomasstewart@gmail.com>
2022-11-20 11:54:59 -0500

    Added the basic inputs for the sign up form

--- src/sign-up/sign-up.jsx
+++ src/sign-up/sign-up.jsx
@@ -1,3 +1,19 @@
+import { Card, TextField, FormControl, Button } from "@mui/material"
+import { Link } from "react-router-dom";
+
 export const SignUp = () => {
-    return <div>Sign up</div>;
+    return <Card variant="outlined">
+        <h1>Login</h1>
+        <FormControl fullWidth>
+            <TextField id="standard-basic" label="username" variant="standard" />
+        </FormControl>
+        <FormControl fullWidth>
+            <TextField id="standard-basic" type="password" label="password" variant="standard" />
+        </FormControl>
+        <FormControl fullWidth>
+            <TextField id="standard-basic" type="password" label="confirm password" variant="standard" />
+        </FormControl>
+        <Link to="/login">Login</Link>
+        <Button>Login</Button>
+    </Card>;
 };
